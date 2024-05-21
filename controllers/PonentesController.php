cdf94f43b601cf8a1b32985bccfe90840239476c
Bryan <bryancg.150601@gmail.com>
2024-05-21 03:45:43 -0600

    Add Ponentes - Form Validation and Save Images

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -2,7 +2,10 @@
 
 namespace Controllers;
 
+use Model\Ponente;
 use MVC\Router;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class PonentesController
 {
@@ -18,10 +21,57 @@ class PonentesController
     public static function crear(Router $router)
     {
         $alertas = [];
+        $ponente = new Ponente;
 
-        $router->render('/admin/ponentes/crear', [
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            // Leer imagen
+            if (!empty($_FILES['imagen']['tmp_name'])) {
+
+                $carpeta_imagenes = '../public/img/speakers';
+
+                // Crear la carpeta si no existe
+                if (!is_dir($carpeta_imagenes)) {
+                    mkdir($carpeta_imagenes, 0755, true);
+                }
+
+                $manager = new ImageManager(new Driver());
+                $imagen_png = $manager->read($_FILES['imagen']['tmp_name'])->cover(800, 800)->toPng();
+                $imagen_webp = $manager->read($_FILES['imagen']['tmp_name'])->cover(800, 800)->toWebp(60);
+                $imagen_avif = $manager->read($_FILES['imagen']['tmp_name'])->cover(800, 800)->toAvif(60);
+
+                $nombre_imagen = md5(uniqid(rand(), true));
+
+                $_POST['imagen'] = $nombre_imagen;
+            }
+            $_POST['redes'] = json_encode($_POST['redes'], JSON_UNESCAPED_SLASHES);
+            $ponente->sincronizar($_POST);
+
+            // validar
+            $alertas = $ponente->validar();
+
+
+            // Guardar el registro
+            if (empty($alertas)) {
+
+                // Guardar las imagenes
+                $imagen_png->save($carpeta_imagenes . '/' . $nombre_imagen . ".png");
+                $imagen_webp->save($carpeta_imagenes . '/' . $nombre_imagen . ".webp");
+                $imagen_avif->save($carpeta_imagenes . '/' . $nombre_imagen . ".avif");
+
+                // Guardar en la BD
+                $resultado = $ponente->guardar();
+
+                if ($resultado) {
+                    header('Location: /admin/ponentes');
+                }
+            }
+        }
+
+        $router->render('admin/ponentes/crear', [
             'titulo' => 'Registrar Ponente',
-            'alertas' => $alertas
+            'alertas' => $alertas,
+            'ponente' => $ponente,
         ]);
     }
 }
