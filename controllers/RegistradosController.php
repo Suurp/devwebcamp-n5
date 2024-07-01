c48b52e1a1539e611657305e11c5d52c227a27d7
Bryan <bryancg.150601@gmail.com>
2024-07-01 00:11:31 -0600

    Add [Admin] - Block Dashboard

--- controllers/RegistradosController.php
+++ controllers/RegistradosController.php
@@ -2,6 +2,10 @@
 
 namespace Controllers;
 
+use Classes\Paginacion;
+use Model\Paquete;
+use Model\Registro;
+use Model\Usuario;
 use MVC\Router;
 
 class RegistradosController {
@@ -11,8 +15,35 @@ class RegistradosController {
             header('Location: /login');
             exit();
         }
+
+        $pagina_actual = $_GET['page'];
+        $pagina_actual = filter_var($pagina_actual, FILTER_VALIDATE_INT);
+
+        if (!$pagina_actual || $pagina_actual < 1) {
+            header('Location: /admin/registrados?page=1');
+            exit();
+        }
+
+        $registros_por_pagina = 5;
+        $total                = Registro::total();
+        $paginacion           = new Paginacion($pagina_actual, $registros_por_pagina, $total);
+
+        if ($paginacion->total_paginas() < $pagina_actual) {
+            header('Location: /admin/registrados?page=1');
+            exit();
+        }
+
+        $registros = Registro::paginar($registros_por_pagina, $paginacion->offset());
+
+        foreach ($registros as $registro) {
+            $registro->usuario = Usuario::find($registro->usuario_id);
+            $registro->paquete = Paquete::find($registro->paquete_id);
+        }
+
         $router->render('/admin/registrados/index', [
-            'titulo' => 'Usuarios Registrados',
+            'titulo'     => 'Usuarios Registrados',
+            'registros'  => $registros,
+            'paginacion' => $paginacion->paginacion(),
         ]);
     }
 }
--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -1,1 +1,21 @@
-<h2 class="dashboard__heading"><?= $titulo ?></h2>
+<h2 class="dashboard__heading"><?=$titulo;?></h2>
+
+<main class="bloques">
+  <div class="bloques__grid">
+    <div class="bloque">
+      <h3 class="bloque__heading">Últimos registros</h3>
+
+      <?php foreach ($registros as $registro) {?>
+        <p class="bloque__texto">
+          <?=$registro->usuario->nombre . " " . $registro->usuario->apellido;?>
+        </p>
+
+      <?php }?>
+    </div>
+
+    <div class="bloque">
+      <h3 class="bloque__heading">Ingresos</h3>
+      <p class="bloque__texto--cantidad">$ <?=$ingresos;?></p>
+    </div>
+  </div>
+</main>
